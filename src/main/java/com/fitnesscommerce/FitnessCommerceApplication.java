a148b627a421e5dee6eb4175b3c1d2708d1064d5
jeongjiwon <jungkt0x@naver.com>
2023-08-16 18:10:22 +0900

    first commit

---
+++ src/main/java/com/fitnesscommerce/FitnessCommerceApplication.java
@@ -0,0 +1,13 @@
+package com.fitnesscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessCommerceApplication.class, args);
+	}
+
+}
