5fbcf0e5843e20e5b5a219088e4c8832515b6ed0
jeongjiwon <jungkt0x@naver.com>
2023-09-07 01:59:10 +0900

    chore: 토큰 응답, 검증 간단 수정
    요청 응답 + Bearer

--- src/main/java/com/fitnesscommerce/domain/auth/dto/response/RefreshResponse.java
+++ src/main/java/com/fitnesscommerce/domain/auth/dto/response/RefreshResponse.java
@@ -8,6 +8,6 @@ public class RefreshResponse {
     private final String accessToken;
 
     public RefreshResponse(String accessToken) {
-        this.accessToken = accessToken;
+        this.accessToken = "Bearer " + accessToken;
     }
 }
