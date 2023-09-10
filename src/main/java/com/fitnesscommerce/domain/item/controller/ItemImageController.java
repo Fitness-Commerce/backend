dc4f5c452e65e7c4a0995a79a71c847b1acf63da
jongsoo <seyeonpark12@naver.com>
2023-09-10 16:51:55 +0900

    feat(swagger): Swagger 구성 파일 추가

--- src/main/java/com/fitnesscommerce/domain/item/controller/ItemImageController.java
+++ src/main/java/com/fitnesscommerce/domain/item/controller/ItemImageController.java
@@ -1,29 +1,34 @@
 package com.fitnesscommerce.domain.item.controller;
 
 import com.fitnesscommerce.domain.item.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "상품 이미지", description = "상품 이미지 관련 API")
 public class ItemImageController {
 
     @Value("${file.storage.location}")
     private String fileStorageLocation;
 
+    @Operation(summary = "상품 이미지 반환", description = "상품 이미지 반환 API")
+    @ApiResponse(responseCode = "200", description = "OK")
     @GetMapping("/api/item/images/{filename:.+}")
     public ResponseEntity<Resource> serveImage(@PathVariable String filename) {
         Path filePath = Paths.get(fileStorageLocation).resolve(filename).normalize();
